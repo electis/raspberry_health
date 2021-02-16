3cf320648870df4b342500fb2c7e4b8727bf0f4e
andrey <esin.andrey@ylab.io>
2021-02-16 16:54:32 +0700

    variables in .env

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import requests
 from dotenv import load_dotenv
 
 LOG = 'logs/health.log'
-IDLE = 30  # tts
-WARNING = (45, 15)  # temp, tts
-CRITICAL = (50, 10)
 load_dotenv(override=True)
 BOSS_ID = os.getenv('BOSS_ID')
 TGRAM_TOKEN = os.getenv('TGRAM_TOKEN')
+WARNING_TEMP = int(os.getenv('WARNING_TEMP', 50))
+CRITICAL_TEMP = int(os.getenv('CRITICAL_TEMP', 60))
+IDLE_SLEEP = int(os.getenv('IDLE_SLEEP', 60))
+WARNING_SLEEP = int(os.getenv('WARNING_SLEEP', 30))
+CRITICAL_SLEEP = int(os.getenv('CRITICAL_SLEEP', 15))
 
 def say2boss(text):
     if not BOSS_ID:
@@ -43,19 +45,19 @@ def main():
             # clock = int(vc.measure_clock('arm')/1000000)
             # print(f'{temp}, {clock}')
 
-            if temp > CRITICAL[0]:
-                tts = CRITICAL[1]
+            if temp > CRITICAL_TEMP:
+                tts = CRITICAL_SLEEP
                 if temp > last_temp:
                     log(f'CRITICAL TEMP: {temp}')
 
-            elif temp > WARNING[0]:
-                tts = WARNING[1]
-                if temp > last_temp or last_temp > CRITICAL[0]:
+            elif temp > WARNING_TEMP:
+                tts = WARNING_SLEEP
+                if temp > last_temp or last_temp > CRITICAL_TEMP:
                     log(f'WARNING TEMP: {temp}')
 
             else:
-                tts = IDLE
-                if last_temp > WARNING[0] or not last_temp:
+                tts = IDLE_SLEEP
+                if last_temp > WARNING_TEMP or not last_temp:
                     log(f'IDLE TEMP: {temp}')
 
             last_temp = temp
